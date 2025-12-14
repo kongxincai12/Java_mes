a42d657eccd1d3edc441cbe51f7f0c97e9752a93
Administrator <1160320205@qq.com>
2025-12-14 18:16:31 +0800

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,8 @@ package org.example;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello World!");
     }
 }
