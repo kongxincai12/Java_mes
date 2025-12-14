53c2b75212ca613e2c67703696585b89340139e3
Administrator <1160320205@qq.com>
2025-12-14 18:40:14 +0800

    no message

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,10 +7,13 @@ public class App {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+<<<<<<< HEAD
 
         System.out.println("Hello World!");
 
         System.out.println("Hello World!");
 
+=======
+>>>>>>> parent of d78eafc (init)
     }
 }
